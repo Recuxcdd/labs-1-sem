a3325d46848e91023dee78e1449a2690a60c16d6
Гусев Егор <egorich127@gmail.com>
2025-09-27 15:38:23 +0300

    лаба 1 залить

---
+++ ML and DS/linear_regression_network.py
@@ -0,0 +1,92 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+k = 0
+b = 0
+X = np.random.normal(50, 70, 10)
+X -= np.min(X) - np.random.randint(0, 250)
+Y = X / 4 + np.random.normal(X, 15) + np.random.rand() % 200
+Y -= np.min(Y) - np.random.randint(0, 250)
+X = np.expand_dims(X, axis=1)
+Y = np.expand_dims(Y, axis=1)
+data_set = np.concatenate((X, Y), axis=1)
+np.random.shuffle(data_set)
+splt = int(len(X) * 0.8)
+X_training = data_set[:splt, 0]
+Y_training = data_set[:splt, 1]
+X_testing = data_set[splt:, 0]
+Y_testing = data_set[splt:, 1]
+
+
+fig, ax = plt.subplots(1, 2, figsize=(8, 4))
+ax[0].scatter(X_training, Y_training)
+line1, = ax[0].plot(X_training, k * X_training + b, "r")
+ax[0].set_title("Training data")
+ax[1].scatter(X_testing, Y_testing)
+line2, = ax[1].plot(X_testing, k * X_testing + b, "r")
+ax[1].set_title("Testing data")
+plt.ion()
+plt.show()
+plt.pause(1)
+
+
+def mse_loss(X, Y, k, b):
+    return np.sum((Y - (k * X + b))**2) / np.size(X)
+
+
+def learn(X, Y, k, b):
+    learn_rate = 0.000001
+    k_deriv = -2 / np.size(X) * np.sum(X * (Y - k * X - b))
+    b_deriv = -2 / np.size(X) * np.sum(Y - k * X - b)
+    k -= k_deriv * learn_rate
+    b -= b_deriv * learn_rate * 1000
+    return k, b
+
+
+def predict(x):
+    return k * x + b
+
+ 
+def predict_from_test():
+    rand_ind = np.random.randint(0, np.size(X_testing) - 1)
+    y_pred = predict(X_testing[rand_ind])
+    temp = [y_pred, Y_testing[rand_ind]]
+    ax[1].scatter(X_testing[rand_ind], Y_testing[rand_ind], color="orange", s=60)
+    plt.pause(0.001)
+    print("*" * 60)
+    print(f"x = {X_testing[rand_ind]}")
+    print(f"y_pred = {y_pred}; y = {Y_testing[rand_ind]}")   
+    print()
+    print(f"accuracy = {round(min(temp) / max(temp), 2)}")
+    print("*" * 60)
+    
+
+def MSE_test_metrik():
+    L = mse_loss(X_testing, Y_testing, k, b)
+    print("*" * 38)
+    print(f"MSE Loss for Testing_data = {L}")
+    print("*" * 38)
+
+
+epochs = 100
+
+print("-" * 38)
+for epoch in range(epochs):
+    print(f"k = {k}; b = {b}")
+    print(f"В {epoch} эпоху MSE_loss = {mse_loss(X_testing, Y_testing, k, b)}")
+    print("-" * 38)
+    k, b = learn(X_training, Y_training, k, b)
+    line1.set_ydata(k * X_training + b)
+    line2.set_ydata(k * X_testing + b)
+    plt.pause(0.001)
+
+
+MSE_test_metrik()
+
+
+plt.pause(999999)
+    
+
+
+
+
--- labi/laba1.py
+++ labi/laba1.py
@@ -3,6 +3,83 @@ import time, sys
 RED = '\u001b[41m'
 BLUE = '\u001b[44m'
 WHITE = '\u001b[47m'
+
+END = "\u001b[0m"
+ERASE = "\x1B[2K"
+BEGIN = "\x1B[1G"
+
+line = " " * 4
+n = 10
+
+
+def flag():
+    for i in range(n):
+        if i < n // 2:
+            print(f"{BLUE}{line * (i + 1)}{WHITE}{line * (n - i)}{END}")
+        else:
+            print(f"{BLUE}{line * (n - i)}{RED}{line * (i + 1)}{END}")
+
+
+def loading():
+    for i in range(100):
+        time.sleep(0.03)
+        print(f"\u001b[100D{i + 1}%", end='', flush=True)
+    print(" Done!")
+
+
+def loading_multiple(n):
+    bar_width = 25
+    for k in range(1, n + 1):
+        for p in range(bar_width  + 1):
+            filled = p
+            bar = "#" * filled + "-" * (bar_width - filled)
+            print(f"{BEGIN}{ERASE}Task {k}/{n} [{bar}] {p * 4}%", 
+                  end='', flush=True)
+            time.sleep(0.1)
+
+
+def draw_line(offset=0, lenght=1, color=88):
+    line = " " * lenght
+    print(f"{" " * offset}\x1b[48;5;{color}m{line}\x1b[0m")
+
+
+def draw_diamond():
+    height = 15
+    lenght = 1
+    center = height // 2
+    offset = height // 2
+    step = 1
+    colors = [88, 157, 165]
+    
+    while True:
+        for color in colors:
+            draw_line(offset, lenght, color)
+            if line < color:
+                offset -= step
+                lenght += step
+            else:
+                offset += step
+                lenght -= step
+        print(f"\x1b[{height + 2}A")
+        print(f"\x1b[{offset}D")
+        lenght = 1
+        offset = height // 2
+        time.sleep(2)
+
+
+
+
+
+
+
+
+
+
+import time, sys
+
+RED = '\u001b[41m'
+BLUE = '\u001b[44m'
+WHITE = '\u001b[47m'
 BLACK = '\u001b[40m'
 
 END = "\u001b[0m"
